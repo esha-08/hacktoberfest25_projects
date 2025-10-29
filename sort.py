8f8d48ddd487a04e7eda3afac42f01f48a89372d
esha-08 <91487493+esha-08@users.noreply.github.com>
2025-10-30 00:30:49 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,14 @@
+# Initializing a list
+a = [5, 1, 5, 6]
+
+# Sort modifies the given list
+a.sort()
+print(a)
+
+b = [5, 2, 9, 6]
+
+# Sorted does not modify the given list
+# and returns a different sorted list
+bs = sorted(b)
+print(b)
+print(bs)
